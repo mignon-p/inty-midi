5c60569152ba116fa929462b9dbc73b77647fd12
Patrick Pelletier <code@funwithsoftware.org>
2019-03-12 12:07:30 -0700

    Dump metadata as comments.

--- Main.hs
+++ Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import Data.Char
+import Data.List
+import Data.Ord
+import Data.Word
 import System.Environment
 import System.Exit
 import System.IO
@@ -7,8 +11,51 @@ import ZMidi.Core.Canonical
 import ZMidi.Core.Datatypes
 import ZMidi.Core.ReadFile
 
+type AbsMidiMessage = (Integer, MidiEvent)
+
+indent :: String
+indent = "    "
+
+getMetaLines :: [AbsMidiMessage] -> [String]
+getMetaLines [] = []
+getMetaLines ((_, MetaEvent (TextEvent typ str)):rest) =
+  trm (indent ++ "' " ++ show typ ++ ": " ++ str) : getMetaLines rest
+  where trm = dropWhileEnd isSpace
+getMetaLines (_:rest) = getMetaLines rest
+
+getMusicLines :: [AbsMidiMessage] -> Either ErrMsg [String]
+getMusicLines _ = Right ["TODO"]
+
+absolutify :: Integer -> [MidiMessage] -> [AbsMidiMessage]
+absolutify _ [] = []
+absolutify now ((delta, ev):rest) =
+  let next = now + fromIntegral delta
+  in (next, ev) : absolutify next rest
+
+combineTracks :: [MidiTrack] -> [AbsMidiMessage]
+combineTracks =
+  sortBy (comparing fst) . concatMap (absolutify 0 . getTrackMessages)
+
+hasOnlyMetadata :: MidiTrack -> Bool
+hasOnlyMetadata trk = onlyMeta $ getTrackMessages trk
+  where onlyMeta [] = True
+        onlyMeta ((_, MetaEvent {}):rest) = onlyMeta rest
+        onlyMeta _ = False
+
+convert' :: Word16 -> [MidiTrack] -> Either ErrMsg [String]
+convert' tpb trks = do
+  let (metadata, music) = partition hasOnlyMetadata trks
+      mdTrk = combineTracks metadata
+      musTrk = combineTracks music
+      metaLines = getMetaLines mdTrk
+  musicLines <- getMusicLines musTrk
+  return $ metaLines ++ musicLines
+
 convert :: MidiFile -> Either ErrMsg [String]
-convert (MidiFile hdr trks) = Right $ map show (getTrackMessages $ trks !! 0)
+convert (MidiFile hdr trks) =
+  case time_division hdr of
+    TPB tpb -> convert' tpb trks
+    _ -> Left "Time division not in ticks-per-beat"
 
 main' :: FilePath -> IO ()
 main' filename = do
