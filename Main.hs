77c5940cbbed19a3f118ff63e7a7cb587adab85a
Patrick Pelletier <code@funwithsoftware.org>
2019-03-12 10:58:52 -0700

    Parse midi file given on command line.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,26 @@
 module Main where
 
+import System.Environment
+import System.Exit
+import System.IO
+import ZMidi.Core.Datatypes
+import ZMidi.Core.ReadFile
+
+main' :: FilePath -> IO ()
+main' filename = do
+  eth <- readMidi filename
+  case eth of
+    Left (ParseErr _ msg) -> do
+      hPutStrLn stderr $ "error parsing " ++ filename ++ ": " ++ msg
+      exitFailure
+    Right midifile -> do
+      putStrLn $ show $ mf_header midifile
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> main' filename
+    _ -> do
+      hPutStrLn stderr "Usage: inty-midi file.mid"
+      exitFailure
