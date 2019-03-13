2d9d6e9fb7866ba5085a5f4a08f0873784dbd388
Patrick Pelletier <code@funwithsoftware.org>
2019-03-12 21:39:24 -0700

    Just combine all the tracks.  Worry about metadata later.

--- Main.hs
+++ Main.hs
@@ -129,12 +129,14 @@ getNotes ((t, VoiceEvent _ (NoteOff ch _ _)):rest) =
   Note t ch (-1) : getNotes rest
 getNotes (_:rest) = getNotes rest
 
+{-
 getMetaLines :: [AbsMidiMessage] -> [String]
 getMetaLines [] = []
 getMetaLines ((_, MetaEvent (TextEvent typ str)):rest) =
   trm (indent ++ "' " ++ show typ ++ ": " ++ str) : getMetaLines rest
   where trm = dropWhileEnd isSpace
 getMetaLines (_:rest) = getMetaLines rest
+-}
 
 getMusicLines :: [AbsMidiMessage] -> Either ErrMsg [String]
 getMusicLines msgs =
@@ -155,26 +157,22 @@ combineTracks :: [MidiTrack] -> [AbsMidiMessage]
 combineTracks =
   sortBy (comparing fst) . concatMap (absolutify 0 . getTrackMessages)
 
+{-
 hasOnlyMetadata :: MidiTrack -> Bool
 hasOnlyMetadata trk = onlyMeta $ getTrackMessages trk
   where onlyMeta [] = True
         onlyMeta ((_, MetaEvent {}):rest) = onlyMeta rest
         onlyMeta _ = False
+-}
 
 convert' :: Word16 -> [MidiTrack] -> Either ErrMsg [String]
-convert' tpb trks = do
-  let (metadata, music) = partition hasOnlyMetadata trks
-      mdTrk = combineTracks metadata
-      musTrk = combineTracks music
-      metaLines = getMetaLines mdTrk
-  musicLines <- getMusicLines musTrk
-  return $ metaLines ++ musicLines
+convert' tpb trks = getMusicLines $ combineTracks trks
 
 convert :: MidiFile -> Either ErrMsg [String]
 convert (MidiFile hdr trks) =
   case time_division hdr of
     TPB tpb -> convert' tpb trks
-    _ -> Left "Time division not in ticks-per-beat"
+    _ -> convert' 384 trks
 
 main' :: FilePath -> IO ()
 main' filename = do
